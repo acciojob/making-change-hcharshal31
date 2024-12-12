12401476f13eb9bc169313446e88174f2ac92124
Harshal Chavan <91681985+hcharshal31@users.noreply.github.com>
2024-12-12 17:01:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,32 @@
 const makeChange = (c) => {
-  // your name here
+	const change = {
+		q: 0,
+		d: 0,
+		n: 0,
+		p: 0
+	}
+	const changeValues = {
+		q: 25,
+		d: 10,
+		n: 5,
+		p: 1
+	}
+
+	change.q = (c - (c % changeValues.q)) / changeValues.q;
+	c = c -changeValues.q;
+
+	change.d = (c - (c % changeValues.d)) / changeValues.d;
+	c = c -changeValues.d;
+
+	change.n = (c - (c % changeValues.n)) / changeValues.n;
+	c = c -changeValues.n;
+
+	change.p = (c - (c % changeValues.p)) / changeValues.p;
+	c = c -changeValues.p;
+
+	return change;
 };
 
 // Do not the change the code below
-const c = prompt("Enter c: ");
-alert(JSON.stringify(makeChange(c)));
+// const c = prompt("Enter c: ");
+// alert(JSON.stringify(makeChange(c)));
